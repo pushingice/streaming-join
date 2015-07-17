fd3144013e508433a88fb44a398564dc0c3ed0d1
Chris Retford <christopher.retford@pearson.com>
2015-07-17 15:49:51 -0600

    Start SQL work

--- src/main/java/com/github/pushingice/impls/sqlite/db/KafkaClient.java
+++ src/main/java/com/github/pushingice/impls/sqlite/db/KafkaClient.java
@@ -1,5 +1,8 @@
 package com.github.pushingice.impls.sqlite.db;
 
+import com.github.pushingice.Message;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
@@ -10,6 +13,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,18 +41,74 @@ public class KafkaClient {
         } catch (IOException e) {
             LOG.error("Problem reading {}", args[0]);
         }
-        Properties kafkaConfig = new Properties();
-        kafkaConfig.setProperty("group.id", "sqlite");
-        kafkaConfig.setProperty("zookeeper.connect", "localhost:2181");
-        Map<String, Integer> topicCountMap = new HashMap<>();
-        ConsumerConnector consumer = kafka.consumer.Consumer
-                .createJavaConsumerConnector(new ConsumerConfig(kafkaConfig));
-        topicCountMap.put("message", 1);
-        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap =
-                consumer.createMessageStreams(topicCountMap);
-        List<KafkaStream<byte[], byte[]>> streams = consumerMap.get("message");
-        KafkaStream<byte[], byte[]> messages = streams.get(0);
-        messages.forEach(x -> LOG.info("{}", new String(x.message())));
+
+        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+        final String columns = "(id integer primary key, " +
+                "fk_id integer, fk_type string)";
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:sqlite:message.db")) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("drop table if exists a");
+            statement.executeUpdate("drop table if exists b");
+            statement.executeUpdate("drop table if exists c");
+            statement.executeUpdate("drop table if exists d");
+            statement.executeUpdate("drop table if exists e");
+            statement.executeUpdate("drop table if exists f");
+            statement.executeUpdate("drop table if exists g");
+            statement.executeUpdate("create table a " + columns);
+            statement.executeUpdate("create table b " + columns);
+            statement.executeUpdate("create table c " + columns);
+            statement.executeUpdate("create table d " + columns);
+            statement.executeUpdate("create table e " + columns);
+            statement.executeUpdate("create table f " + columns);
+            statement.executeUpdate("create table g " + columns);
+
+            Properties kafkaConfig = new Properties();
+            kafkaConfig.setProperty("group.id", "sqlite");
+            kafkaConfig.setProperty("zookeeper.connect", "localhost:2181");
+            Map<String, Integer> topicCountMap = new HashMap<>();
+            ConsumerConnector consumer = kafka.consumer.Consumer
+                    .createJavaConsumerConnector(
+                            new ConsumerConfig(kafkaConfig));
+            topicCountMap.put("message", 1);
+            Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap =
+                    consumer.createMessageStreams(topicCountMap);
+            List<KafkaStream<byte[], byte[]>> streams =
+                    consumerMap.get("message");
+            KafkaStream<byte[], byte[]> messages = streams.get(0);
+            messages.forEach(x -> {
+                String msgContent = new String(x.message());
+                Message msg = gson.fromJson(msgContent, Message.class);
+                LOG.info("{}", msg);
+                try {
+                    statement.executeUpdate(String.format(
+                            "INSERT INTO %s VALUES (%d, %d, '%s')",
+                            msg.getMessageType(), msg.getId(), msg.getFkId(),
+                            msg.getFkMessageType()));
+                } catch (SQLException e) {
+                    LOG.error("{}", e.getMessage());
+                    if (e.getMessage().startsWith("UNIQUE constraint")) {
+                        try {
+                            statement.executeUpdate(
+                                    String.format("UPDATE %s SET " +
+                                                    "fk_id = %d, fk_type='%s' " +
+                                                    "WHERE id = %d ",
+                                            msg.getMessageType(),
+                                            msg.getFkId(),
+                                            msg.getFkMessageType(),
+                                            msg.getId())
+                                    );
+                        } catch (SQLException e1) {
+                            LOG.error("{}", e1.getMessage());
+                        }
+                    }
+
+                }
+            });
+
+        } catch (SQLException e) {
+            LOG.error("{}", e.getMessage());
+        }
 
     }
 }
